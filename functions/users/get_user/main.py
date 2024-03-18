28b793d39ee9d15215dfa0e8de20caad21b1bb77
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2024-03-18 01:14:37 -0300

    creating users crud

--- functions/users/get_user/main.py
+++ functions/users/get_user/main.py
@@ -28,4 +28,5 @@ def lambda_handler(event, context):
     user_id = event["pathParameters"].get("user_id")
     user = users_table.get_item(Key={"PK": user_id}).get("Item")
 
+    user = { "id": user["PK"], "name": user["name"], "age": user["age"]}
     return {"statusCode": 200, "body": json.dumps(user,  default=str)}
