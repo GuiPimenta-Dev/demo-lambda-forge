298a7025a088e46114468e7e40b5521fceef10f5
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2024-03-18 22:39:27 -0300

    test

--- infra/stacks/dev_stack.py
+++ infra/stacks/dev_stack.py
@@ -21,7 +21,7 @@ class DevStack(cdk.Stack):
                 "Synth",
                 input=source,
                 install_commands=[
-                    "pip install lambda-forge==1.0.82 --extra-index-url https://pypi.org/simple --extra-index-url https://test.pypi.org/simple/",
+                    "pip install lambda-forge==1.0.84 --extra-index-url https://pypi.org/simple --extra-index-url https://test.pypi.org/simple/",
                     "pip install aws-cdk-lib",
                     "npm install -g aws-cdk",
                 ],
