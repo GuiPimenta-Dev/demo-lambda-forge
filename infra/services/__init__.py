075025c9513e182e6246744d4c6f1882ec982743
Guilherme Alves Pimenta <guialvespimenta27@gmail.com>
2024-03-22 09:23:32 -0300

    readding lfunctions

--- infra/services/__init__.py
+++ infra/services/__init__.py
@@ -1,8 +1,10 @@
 from infra.services.api_gateway import APIGateway
 from infra.services.aws_lambda import AWSLambda
+from infra.services.dynamo_db import DynamoDB
 
 
 class Services:
     def __init__(self, scope, stage, arns) -> None:
         self.api_gateway = APIGateway(scope, stage)
         self.aws_lambda = AWSLambda(scope, stage)
+        self.dynamo_db = DynamoDB(scope, arns)
